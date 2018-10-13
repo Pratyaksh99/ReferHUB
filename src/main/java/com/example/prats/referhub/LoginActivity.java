8e14e067afbae524ec250d4b46c6143a000a51e9
Kaushik2910 <32077397+Kaushik2910@users.noreply.github.com>
2018-10-13 11:03:39 -0400

    Add files via upload

--- src/main/java/com/example/prats/referhub/LoginActivity.java
+++ src/main/java/com/example/prats/referhub/LoginActivity.java
@@ -1,13 +1,80 @@
-package com.example.prats.referhub;
-
-import android.support.v7.app.AppCompatActivity;
+package com.example.kaushik.referhub;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText email,password;
+    Button registerButton,loginButton;
+    FirebaseAuth firebaseAuth;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.Email);
+        password = (EditText) findViewById(R.id.Password);
+        registerButton = (Button) findViewById(R.id.Signup);
+        loginButton = (Button) findViewById(R.id.ForgotPassword);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email1 = email.getText().toString()   ;
+                String password1 = password.getText().toString();
+
+                if(TextUtils.isEmpty(email1)){
+                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password1)){
+                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                }
+
+                if(password1.length()<6){
+                    Toast.makeText(getApplicationContext(),"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email1,password1)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(getApplicationContext(),"E-mail or password is wrong",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
+        if(firebaseAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
     }
 }
